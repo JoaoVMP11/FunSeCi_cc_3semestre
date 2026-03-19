d46131fb0dee6fa2454fe2fa7ce18cd4eeade470
João Vitor Mendes Peres <joao.mperes@dinfor.uniceub>
2026-03-19 10:01:34 -0300

    Add aes

---
+++ aulaprojects/baula6/src/baula6/AES.java
@@ -0,0 +1,51 @@
+package baula6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+	private static String encriptar (String texto, String chave) throws Exception {
+		Cipher objCifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "AES");
+		IvParameterSpec objIv = new IvParameterSpec("0123456789101112".getBytes());
+		objCifra.init(Cipher.ENCRYPT_MODE, objChave, objIv);
+		byte[] vetor = objCifra.doFinal(texto.getBytes("UTF-8"));
+		return Base64.getEncoder().encodeToString(vetor);
+	}
+	
+	private static String decriptar (String cifra, String chave) throws Exception {
+		Cipher objCifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		SecretKey objChave = new SecretKeySpec(chave.getBytes("UTF-8"), "AES");
+		IvParameterSpec objIv = new IvParameterSpec("0123456789101112".getBytes());
+		objCifra.init(Cipher.DECRYPT_MODE, objChave, objIv);
+		byte[] vetor = objCifra.doFinal(Base64.getDecoder().decode(cifra));
+		return new String(vetor, "UTF-8");
+	}
+	
+	public static void main(String[] args) {
+		BufferedReader leitor = new BufferedReader (new InputStreamReader(System.in));
+		
+		try {
+			System.out.print("digite um texto: ");
+			String texto = leitor.readLine();
+			
+			System.out.print("Digite uma chave: ");
+			String chave = leitor.readLine();
+			
+			String cifra = encriptar(texto, chave);
+			
+			System.out.println(cifra);
+			System.out.println(decriptar(cifra, chave));
+			
+		} catch (Exception erro) {
+			System.out.println(erro);
+		}
+	}
+
+}
